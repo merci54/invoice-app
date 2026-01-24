d11df12fe7d62fc4f89c4cf74d1f159b280c7a38
Yaroslav <yaroslavlit@gmail.com>
2026-01-24 12:14:18 +0100

    add nothing component

---
+++ components/NothingPage/NothingPage.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import css from './NothingPage.module.scss';
+
+export default function NothingPage() {
+    return <div className={css.wrapper}>
+        <Image className={css.image} src={'/nothing.svg'} alt='Image nothing' width={193} height={160} />
+        <div className={css.textBlock}>
+            <h2 className={css.title}>
+                There is nothing here
+            </h2>
+            <p className={css.text}>Create an invoice by clicking the 
+                New button and get started</p>
+        </div>
+    </div>
+}
